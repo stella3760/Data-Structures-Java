9f662405681fd4b4ef97edd52f5734b2caf99c76
Seunghye.Stella.Kim <120572485+stella3760@users.noreply.github.com>
2023-07-11 11:43:18 +0100

    Queue

---
+++ ArrayList/src/TestArrayList.java
@@ -0,0 +1,20 @@
+/* @author seunghyekim */
+
+import java.util.*;
+
+public class TestArrayList {
+    
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        java.util.ArrayList<ArrayList> list;
+
+        list = new java.util.ArrayList<ArrayList>();
+
+        ArrayList p = new ArrayList (" John", 20);
+        
+        
+    }
+}
